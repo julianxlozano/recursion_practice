b98b60a3ebd8b1b5c76cbde638a579d074138ac5
julianxlozano <julianxlozano@gmail.com>
2021-07-23 15:22:33 -0500

    solved first problem

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution here!
 
+function printString(string){
+    if(string.length === 1){
+        console.log(string[0])
+    }else{
+        console.log(string[0])
+        let new_string = string.substring(1)
+        printString(new_string)
+    }
+}
+
